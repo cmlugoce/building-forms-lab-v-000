91f69849ec0c92ebd88193416fb84be997437b2b
Cristina Lugo <cmlugoce@syr.edu>
2018-09-11 14:38:48 +0000

    automatically backed up by learn

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,6 +2,22 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+
+   state = {
+    name: ''
+  }
+   handleChange = event => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+   handleSubmit = event => {
+    event.preventDefault();
+    this.props.addBand(this.state);
+    this.setState({
+      name: ''
+    })
+  }
   render() {
     return(
       <div>
