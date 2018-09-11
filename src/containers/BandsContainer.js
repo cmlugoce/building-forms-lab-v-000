da64f04db78ac370d68bf8919fe7fa61683d2561
Cristina Lugo <cmlugoce@syr.edu>
2018-09-11 14:40:20 +0000

    automatically backed up by learn

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Bands from '../components/Bands';
+import BandInput from '../components/BandInput';
 class BandsContainer extends Component {
   render() {
     return(
